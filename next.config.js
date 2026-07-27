7ba674da1c8ea505e3c8dbae6c813aeb5d2f6713
opeeteedev <nexusgrowthss@gmail.com>
2026-07-27 19:34:54 +0100

    Add Next.js configuration with headers and redirects

---
+++ next.config.js
@@ -0,0 +1,50 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/linkedin',
+        destination: 'https://linkedin.com/in/opeyemi-omotayo',
+        permanent: false,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/opeeteedev',
+        permanent: false,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
